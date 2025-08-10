51f7fc418d11e4fc1964c2c850c178d3353156a1
Kevin Hartono <kevinhartono2810@gmail.com>
2025-08-10 16:47:36 +0700

    feat(Transactions): model

---
+++ backend/models/Transaction.js
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const Transaction = sequelize.define('Transaction', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  amount: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: false,
+});
+
+export default Transaction;
