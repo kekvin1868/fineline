a77007a8698d125b8a09c2cd0e05eedddab1a692
Kevin Hartono <kevinhartono2810@gmail.com>
2025-08-11 22:35:34 +0700

    feat(User): model

---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+const User = sequelize.define ('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+}, {
+  timestamps: false,
+});
